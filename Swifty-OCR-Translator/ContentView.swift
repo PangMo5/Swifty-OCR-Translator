8e8e28e33d5658c50158a01a4a155d5870e67e9b
PangMo5 <a3457709@icloud.com>
2021-07-06 18:44:07 +0900

    Initial Commit

---
+++ Swifty-OCR-Translator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty-OCR-Translator
+//
+//  Created by PangMo5 on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifty-OCR-Translator/Swifty_OCR_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_OCR_TranslatorApp.swift
+//  Swifty-OCR-Translator
+//
+//  Created by PangMo5 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_OCR_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
